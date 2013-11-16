832018a08fe3c14281ad5eece493b8781ebe9301
Arthur Bikmullin <devolonter@gmail.com>
2013-11-16 19:58:48 +0400

    Added requestAnimationFrame polyfill

--- mojo.html5.gl.js
+++ mojo.html5.gl.js
@@ -856,6 +856,13 @@ var mojoHtml5Gl = function(undefined){
 	}
 
 	function init(id) {
+		var vendors = ['ms', 'moz', 'webkit', 'o'];
+		for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+			window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+			window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+				|| window[vendors[x]+'CancelRequestAnimationFrame'];
+		}
+
 		if (window.WebGLRenderingContext !== undefined) {
 			var mojoPatch = undefined;
 
